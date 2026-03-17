7e1c037a0fa16e7226226818b32561615b4a3828
barukponce <barukponce@gmail.com>
2026-03-17 17:46:05 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Flavia, eu amo você, se você me ama, me da um beijinho!</h1>
+    return <h1>Flavia, eu amo você. Se você me ama, me da uma risadinha!</h1>
 }
 
 export default Home; 
